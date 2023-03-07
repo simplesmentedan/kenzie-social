a4bff6e15d23fb6eccb6306659cc987b7787435a
Lucas Cruz <lucascruz0103@gmail.com>
2023-03-07 15:59:11 -0300

    -Add:adicionando as funções de cadastro e login

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Usuario(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
 
 # Create your models here.
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Usuario
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import UserSerializer
+from .permissions import IsAccountOwner
 
-# Create your views here.
+
+class UserView(generics.CreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = Usuario.objects.all()
+    serializer_class = UserSerializer
+
+
