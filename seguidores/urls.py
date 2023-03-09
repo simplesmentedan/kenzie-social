a167ed7ad2a05e38bdf7430717862c7258747ce8
Steiner <dssteinerstt@gmail.com>
2023-03-09 13:05:37 -0300

    add: adicionar a possibilidade de buscar todos os seguidores do usuário

--- seguidores/urls.py
+++ seguidores/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("seguidores/", views.SeguidoresView.as_view()),
     path("seguidores/<int:pk>", views.SeguidoresView.as_view()),
 ]
